57b6098610db3f6676220e71449de9c5efcc7265
luv <luv@henoida.com>
2016-10-23 13:00:51 +0530

    adds a condition check instead of assert

--- Referee/pingpong/blueprints/player/service.py
+++ Referee/pingpong/blueprints/player/service.py
@@ -27,14 +27,12 @@ def draw_next_round(round_num):
 			player.state = State.playing
 			winning_players.append(player)
 
-	print '##############################################',max_rounds, 'round_num', round_num
-	if max_rounds >= round_num:
-		assert len(winning_players) > 1
+	if len(winning_players) and max_rounds >= round_num:
 		random.shuffle(winning_players)
 		# select using zip players in even index with odd
 		draws = zip(winning_players[::2], winning_players[1::2])
 		for d in draws:
-			print "Game draws", d[0].username, d[1].username
+			# print "Game draws", d[0].username, d[1].username
 			game = Game( round_num, d[0], d[1])		
 		return len(draws)
 		
