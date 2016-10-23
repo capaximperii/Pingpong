25b73a15ccf180e0187ef82a3c313ebb3e07aafd
luv <luv@henoida.com>
2016-10-23 12:57:37 +0530

    adds page tests

--- Referee/pingpong/tests/page/test_views.py
+++ Referee/pingpong/tests/page/test_views.py
@@ -1,8 +1,26 @@
 from flask import url_for
 
 
-class TestPage(object):
-    def test_home_page(self, client):
-        """ Home page should respond with a success 200. """
-        response = client.get('/')
-        assert response.status_code == 200
+def test_home_page(client):
+    """ Home page should respond with a success 200. """
+    response = client.get('/')
+    assert response.status_code == 200
+
+def test_login(client):
+	""" Check for authentication for valid user. """ 
+	data = { 'username': 'Joey', 'password': 'Joey'}
+	response = client.post('/api/authentication', data=data)
+	assert response.status_code == 200
+
+def test_bad_login(client):
+	""" Check for authentication for valid user. """ 
+	data = { 'username': 'Joey', 'password': 'oey'}
+	response = client.post('/api/authentication', data=data)
+	assert response.status_code == 401
+
+def test_logout(client):
+	data = { 'username': 'Joey', 'password': 'Joey'}
+	response = client.post('/api/authentication', data=data)
+	assert response.status_code == 200
+	response = client.get('/api/logout')
+	assert response.status_code == 200
