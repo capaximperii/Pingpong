b77229422f514a4b7f16771f0db016982ce74e49
luv <luv@henoida.com>
2016-10-23 10:05:20 +0530

    Updates settings.

--- Referee/config/settings.py
+++ Referee/config/settings.py
@@ -1,10 +1,6 @@
-from datetime import timedelta
-
 DEBUG = True
 
 SERVER_NAME = 'localhost:8000'
-SECRET_KEY = 'badpassword'
-
 
 #	The players allowed along with, with ids as keys.
 
@@ -13,12 +9,5 @@ PLAYERS = 	{
 				4: 'Vivek', 5: 'Pritam', 6: 'Amit',
 				7: 'Chandler', 8: 'Colwin' 
 			}
-
-
-#	These ids match the index within the defense array for each player
-# 	The number is 0th place is a place holder to match 
-DEFENSE = 	[ 0, 8, 8, 7, 7, 6, 6, 5, 5  ]
-
-REMEMBER_COOKIE_DURATION = timedelta(days=90)
 
 MAX_SCORE = 5
