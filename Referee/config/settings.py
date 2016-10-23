3c63e85ef29b0ec3107e7629aeaf9b79d3308942
luv <luv@henoida.com>
2016-10-23 12:56:22 +0530

    updates settings for unit tests

--- Referee/config/settings.py
+++ Referee/config/settings.py
@@ -2,8 +2,9 @@ DEBUG = True
 
 SERVER_NAME = 'localhost:8000'
 
-#	The players allowed along with, with ids as keys.
+SECRET_KEY = 'badpassword'
 
+#	The players allowed along with, with ids as keys.
 PLAYERS = 	{
 				1: 'Joey', 2: 'Nick', 3: 'Russel', 
 				4: 'Vivek', 5: 'Pritam', 6: 'Amit',
