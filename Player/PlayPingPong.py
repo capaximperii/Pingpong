aa3c41a158a571a134005edffa6e25252440c552
luv <luv@henoida.com>
2016-10-23 10:04:55 +0530

    Adds config file to clients.

--- Player/PlayPingPong.py
+++ Player/PlayPingPong.py
@@ -1,11 +1,24 @@
 from championship import Player
+from ConfigParser import ConfigParser
 import getopt
 import sys
+import os
 
 if __name__ == '__main__':
 	opts, args = getopt.getopt(sys.argv[1:],"u:",["username="])
 	for opt, arg in opts:
+
 		if opt in ("-u", "--username"):
-			u = arg
-			player = Player(u.strip(), u.strip())
-			player.play()
+			u = arg.strip().lower()
+			cfg = ConfigParser()
+			cfg.read(os.path.join("config", "players.ini"))
+			# Check if the player has a configuration
+			if u in cfg.sections():
+				username = cfg.get(u, 'username')
+				password = cfg.get(u, 'password')
+				defense = cfg.get(u, 'defense')
+				url = cfg.get(u, 'url')
+				player = Player(username, password, defense, url)
+				player.play()
+			else:
+				print u, 'not in ', cfg.sections()
--- Player/championship/Session.py
+++ Player/championship/Session.py
@@ -4,17 +4,19 @@ import json
 import time
 import random
 
-URL = "http://localhost:8000"
 
 class Player:
-	def __init__(self, username, password):
+		
+	def __init__(self, username, password, defense, url):
 		self.username = username
 		self.password = password
+		self.defense = int(defense)
 		self.session = requests.session()
-		self.payload = self.session.get(URL)
+		self.url = url
+		self.payload = self.session.get(url)
 
 	def authenticate(self):
-		self.payload = self.session.post(URL + "/api/authentication",
+		self.payload = self.session.post(self.url + "/api/authentication",
 							data={	"username": 	self.username ,
 									"password":	self.password}
 							)
@@ -26,34 +28,35 @@ class Player:
 		return self.payload.json()
 
 	def keepalive(self):
-		self.payload = self.session.get(URL + "/api/keepalive")
+		self.payload = self.session.get(self.url + "/api/keepalive")
 		if self.payload.status_code != 200:
 			return None
 		return self.payload.json()
 
 	def checkgame(self):
-		self.payload = self.session.get(URL + "/api/checkgame")
+		self.payload = self.session.get(self.url + "/api/checkgame")
 		if self.payload.status_code != 200:
 			return False
 		return self.payload.json()
 
 	def attack(self, gid):
 		number_picked = random.randrange(1, 11)
-		self.payload = self.session.put(URL + "/api/attack/" + str(gid),
+		self.payload = self.session.put(self.url + "/api/attack/" + str(gid),
 					data={	"number_picked": 	number_picked })
 		return self.payload.json()
 
-	def defend (self, gid, n):
+	def defend (self, gid):
 		number_picked = []
-		for i in range(0, n):
+		for i in range(0, self.defense):
+			# Game rules mention nothing about duplicate numbers in defense.
 			number_picked.append(random.randrange(1, 11))
 
-		self.payload = self.session.put(URL + "/api/defend/" + str(gid),
+		self.payload = self.session.put(self.url + "/api/defend/" + str(gid),
 					data={	"number_picked": 	json.dumps(number_picked) })
 		return self.payload.json()
 
 	def logout(self):
-		self.payload = self.session.get(URL + "/api/logout")
+		self.payload = self.session.get(self.url + "/api/logout")
 		return self.payload.json()
 
 	
@@ -71,9 +74,8 @@ class Player:
 				gid = response['gid']
 				response = self.attack(gid)
 			elif command == 'defend':
-				defense = response['defense']
 				gid = response['gid']
-				response = self.defend(gid, defense)
+				response = self.defend(gid)
 			elif command == 'logout':
 				response = self.logout()
 				break
